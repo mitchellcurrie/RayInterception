e5aeced642cd93742e08e8d2fd6f661f4f766010
Mitchell Currie <mitchell.jc@hotmail.com>
2018-10-24 16:53:23 +0100

    Object loading working - load function storing result in ptr - takes 43 seconds to load

--- RayInterception/RayInterception/main.cpp
+++ RayInterception/RayInterception/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <time.h>
+#include "ObjectData.h"
+#include "ObjectLoader.h"
 
 int main()
 {
+	// Load Object
+
+	ObjectLoader* objLoader = new ObjectLoader();
+	
+	ObjectDataPtr objPtr(nullptr);
+
+	//clock_t tStart = clock();
+
+	objPtr = objLoader->Load("green.obj");
+
+	//printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+
+
+
+	int i;
+	std::cin >> i;
+
+
+	
 	return 0;
 }
